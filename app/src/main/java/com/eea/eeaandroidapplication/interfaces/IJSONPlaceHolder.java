4ddb70984d4730ae92f3d1c63d56a4ba7891ceaf
dumidubanda <61262969+dumidubanda@users.noreply.github.com>
2022-01-18 16:03:26 +0530

    Minor change
    Minor change

--- app/src/main/java/com/eea/eeaandroidapplication/interfaces/IJSONPlaceHolder.java
+++ app/src/main/java/com/eea/eeaandroidapplication/interfaces/IJSONPlaceHolder.java
@@ -15,20 +15,6 @@ public interface IJSONPlaceHolder
 {
 
 
-//    @GET("users")
-//    Call<List<User>> getUsers();
-//
-//    @GET("getbyid/{id}")
-//    Call<Employee> getEmployee(@Path("id") int empId);
-//
-//    @POST("saveemployee")
-//    Call<ResponseBody> saveEmployee(@Body Employee employee);
-//
-//    @PUT("updateemployee/{id}")
-//    Call<ResponseBody> updateEmployee(@Path("id") int empId,@Body Employee employee);
-
-    @DELETE("deleteemployee/{id}")
-    Call<ResponseBody> deleteEmployee(@Path("id") int empId);
 
     @POST("savecustomer")
     Call<ResponseBody> saveCustomer(@Body Customer customer);
